c69c6971c765ccf5a7176ae325c6d6d25ab7d667
sibinnediyoram <sibinnediyoram@gmail.com>
2018-10-30 15:04:13 +0530

    order of functions shanged

--- server_monitor.py
+++ server_monitor.py
@@ -1,5 +1,24 @@
 import psutil
 from os import popen
+
+def memory_stat():
+    """ We can get the memory status details using free -t -m , command with popen module """
+    total_Memory, used_Memory, free_Memory = map(int, popen('free -t -m').readlines()[-1].split()[1:])
+    return 'Total Memory = ' + str(total_Memory) + ' MB\n'+'Used Memory = ' + str(used_Memory) + ' MB\n'+'Free Memory = ' + str(free_Memory) + ' MB'
+
+def getDiskSpace():
+    p = popen("df -h /")
+    i = 0
+    while 1:
+        i = i +1
+        line = p.readline()
+        if i==2:
+            return(line.split()[1:5])
+
+disk_Stat = getDiskSpace()
+disk_Total, disk_Used, disk_Free = disk_Stat[0], disk_Stat[1], disk_Stat[2]
+current_diskusage = 'Total Disk Space= ' + str(disk_Total) + '\n' + 'Free Disk Space available= ' + str(disk_Free) +'\n' + 'Total Disk Space Used= ' + str(disk_Used)
+
 def system_busy():
     """
     :param float threshold: relative threshold (load_1min/num_cpus) past which the system is considered busy
@@ -44,25 +63,7 @@ def system_busy():
     else:
         print "your server cpu load is normal"
 
-#    return server_Loadaverage
-
-def memory_stat():
-    """ We can get the memory status details using free -t -m , command with popen module """
-    total_Memory, used_Memory, free_Memory = map(int, popen('free -t -m').readlines()[-1].split()[1:])
-    return 'Total Memory = ' + str(total_Memory) + ' MB\n'+'Used Memory = ' + str(used_Memory) + ' MB\n'+'Free Memory = ' + str(free_Memory) + ' MB'
-
-def getDiskSpace():
-    p = popen("df -h /")
-    i = 0
-    while 1:
-        i = i +1
-        line = p.readline()
-        if i==2:
-            return(line.split()[1:5])
-
-disk_Stat = getDiskSpace()
-disk_Total, disk_Used, disk_Free = disk_Stat[0], disk_Stat[1], disk_Stat[2]
-current_diskusage = 'Total Disk Space= ' + str(disk_Total) + '\n' + 'Free Disk Space available= ' + str(disk_Free) +'\n' + 'Total Disk Space Used= ' + str(disk_Used)
+# Check server load and other stats now
 system_busy()
 
 
