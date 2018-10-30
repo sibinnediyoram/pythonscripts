230a125df6c54ab248220f4e89dec5b1a419212b
sibinnediyoram <sibinnediyoram@gmail.com>
2018-10-30 10:06:37 +0530

    added mail feature

--- server_monitor.py
+++ server_monitor.py
@@ -1,21 +1,50 @@
 import psutil
 from os import popen
-def is_system_busy():
+def system_busy():
     """
-    :param float threshold: relative threshold (load_15min/num_cpus) past which the system is considered busy
+    :param float threshold: relative threshold (load_1min/num_cpus) past which the system is considered busy
     :return: True if the system is considered busy; False otherwise
     :rtype: bool
     """
     from psutil import cpu_count
     from os import getloadavg
-    threshold = 2
+    import smtplib
+    from socket import gethostname
+    import string
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    threshold = float(0.2)
     load_1, load_5, load_15 = getloadavg()
     server_Loadaverage = str(getloadavg())
-    if load_15 / psutil.cpu_count() >= threshold:
+    if load_1 / psutil.cpu_count() >= threshold:
+        HOST_NAME = gethostname()
+        FROM = "hostdimetestmail@gmail.com"
+        MY_PASSWORD = "Test@123456"
+        TO = "sibin.j@hostdime.in"
+        SUBJECT = "server cpu overloaded"
         print "server is having high cpu load"
+        # Next, log in to the mail server
+
+        # Send the mail
+        TEXT = HOST_NAME + '\n' + "cpu overloaded" + '\n' + "Current load is: " + '\n' + server_Loadaverage + '\n' + "memory status is: "+ '\n' + memory_stat() + '\n' + "Disk status is: " + '\n' + current_diskusage
+        message = """\
+        From: %s  
+        
+        Subject: %s
+        
+        %s
+        """ % (FROM, SUBJECT, TEXT)
+        try:
+            server.ehlo()
+            server.starttls()
+            server.login(FROM, MY_PASSWORD)
+            server.sendmail(FROM, TO, message)
+            server.close()
+        except:
+            print 'Mail sending failed...'
     else:
         print "your server cpu load is normal"
-    return server_Loadaverage
+
+#    return server_Loadaverage
 
 def memory_stat():
     """ We can get the memory status details using free -t -m , command with popen module """
@@ -30,11 +59,13 @@ def getDiskSpace():
         line = p.readline()
         if i==2:
             return(line.split()[1:5])
+
+
 disk_Stat = getDiskSpace()
 disk_Total, disk_Used, disk_Free = disk_Stat[0], disk_Stat[1], disk_Stat[2]
-print is_system_busy()
-print memory_stat()
-print 'Total Disk Space= ' + str(disk_Total) + '\n' + 'Free Disk Space available= ' + str(disk_Free) +'\n' + 'Total Disk Space USed= ' + str(disk_Used)
+current_diskusage = 'Total Disk Space= ' + str(disk_Total) + '\n' + 'Free Disk Space available= ' + str(disk_Free) +'\n' + 'Total Disk Space Used= ' + str(disk_Used)
+system_busy()
+
 
 
 
